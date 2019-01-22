d989e51d96d635b9d95bcf074b551dc6d2adabf8
KieranVieira <kieranvieira@live.com>
2019-01-22 17:23:33 -0500

    Finished mac subnav

--- apple-nav/src/components/SubNav/SubNav.js
+++ apple-nav/src/components/SubNav/SubNav.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const SubNavContainer = styled.div`
     display: flex;
-    justify-content: space-around;
+    justify-content: space-evenly;
     width: 100%;
     height: 92px;
     padding: 12px 0;
     background-color: #F6F6F6;
+    div{
+        display: flex;
+        margin-top: 1px;
+        a{
+            text-decoration: none;
+            color: black;
+            &:hover{
+            color: #0070c9;
+            }
+        }
+    }
+    .sub-active{
+        color: #0070c9;
+    }
 `; 
 
 const Product = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    margin-top: -9px;
+    margin: -9px 20px 0 20px;
     h2{
         margin-top: 2px;
         font-size: 11px;
@@ -29,6 +44,7 @@ const Product = styled.div`
         margin: 0 auto;
     }
     p{
+        height: 15px;
         font-size: 9px;
         margin: unset;
         margin-top: -9px;
@@ -41,52 +57,62 @@ const products = {
         {
             name: 'MacBook',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macbook__fjd32xo0xwmm_large.svg',
-            subName: ''
+            subName: '',
+            path: 'macbook'
         },
         {
             name: 'MacBook Air',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookair__by1vem70tkc2_large.svg',
-            subName: 'New'
+            subName: 'New',
+            path: 'macbook-air'
         },
         {
             name: 'MacBook Pro',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookpro__e0wcugzmt26a_large.svg',
-            subName: ''
+            subName: '',
+            path: 'macbook-pro'
         },
         {
             name: 'iMac',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/imac__dlz2ciyr6hm6_large.svg',
-            subName: ''
+            subName: '',
+            path: 'imac'
         },
         {
             name: 'iMac Pro',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/imac_pro__el8kcar06qky_large.svg',
-            subName: ''
+            subName: '',
+            path: 'imac-pro'
         },
         {
             name: 'Mac Pro',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macpro__dt69qr0ywncm_large.svg',
-            subName: ''
+            subName: '',
+            path: 'mac-pro'
         },
         {
             name: 'Mac mini',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macmini__rzlk7b0xsmqq_large.svg',
-            subName: 'New'
+            subName: 'New',
+            path: 'mac-mini'
         },
         {
             name: 'Accessories',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_acc__fia3ihqup4ae_large.svg',
-            subName: ''
+            subName: '',
+            path: 'accessories'
         },
         {
             name: 'Mojave',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_osx__dq0x03ehapyu_large.svg',
-            subName: ''
+            subName: '',
+            path: 'mojave'
         },
         {
             name: 'Compare',
             icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_comp__by57zw2zt32a_large.svg',
-            subName: ''
+            subName: '',
+            path: 'compare'
         }
     ]
 }
@@ -95,15 +121,19 @@ const SubNav = props => {
     const productLine = props.match.params.productLine;
     return(
         <SubNavContainer>
+            <div>
             {products[productLine].map(product => {
                 return(
-                    <Product icon={product.icon}>
-                        <figure></figure>
-                        <h2>{product.name}</h2>
-                        <p>{product.subName}</p>
-                    </Product>
+                    <NavLink to={`/${productLine}/${product.path}`} activeClassName='sub-active'>
+                        <Product icon={product.icon}>
+                            <figure></figure>
+                            <h2>{product.name}</h2>
+                            <p>{product.subName}</p>
+                        </Product>
+                    </NavLink>
                 )
-            })}
+            })} 
+            </div>
         </SubNavContainer>
     )
 }
